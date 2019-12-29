eb603a5110269006019a0218752c9aac4e8d201e
csustjohnny  email = 962893304@qq.com <962893304@qq.com>
2019-12-29 19:55:45 +0800

    extract github config from code to properties

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,9 +1,7 @@
 package life.majiang.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Lenovo
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        //model.addAttribute("name",name);
         return "index";
     }
 }

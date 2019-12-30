e35a8ae582f3e6c4f6bdefe530f02c87ca67d062
csustjohnny  email = 962893304@qq.com <962893304@qq.com>
2019-12-30 15:17:56 +0800

    add check cookies

--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package life.majiang.community.mapper;
 import life.majiang.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author Lenovo
@@ -16,4 +17,6 @@ public interface UserMapper {
     @Insert("Insert into user (name, account_id, token, gmt_create, gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("Select * from user where token = #{token}")
+    User findByToken(String token);
 }
